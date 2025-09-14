c725041d3334896e611c7728995d96e055b16e25
HOLSEN MOORE <hols3nmoore@gmail.com>
2025-09-14 17:51:30 -0500

    Add medicine curing evnts

---
+++ src/main/java/com/holsfastagz/plaguedelight/events/MedicineEvents.java
@@ -0,0 +1,75 @@
+package com.holsfastagz.plaguedelight.events;
+
+import com.holsfastagz.plaguedelight.capability.ImmunityCapability;
+import com.holsfastagz.plaguedelight.registry.ModEffects;
+import com.holsfastagz.plaguedelight.registry.ModItems;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+
+@Mod.EventBusSubscriber(modid = "plaguedelight")
+public class MedicineEvents {
+
+    // Note that antivirus is an effect rather than an event
+
+    // Cure bacterial infections with penicillin (except scarlet fever)
+    @SubscribeEvent
+    public static void onPenicillinConsumed(LivingEntityUseItemEvent.Finish event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+    
+        // Check penicillin 
+        if (event.getItem().getItem() == ModItems.PENICILLIN.get()) {
+
+            // 20% chance
+            if (player.level().random.nextFloat() < 0.20F) {
+                player.removeEffect(ModEffects.ANTHRAX.get());
+                player.removeEffect(ModEffects.DYSENTERY.get());
+                player.removeEffect(ModEffects.PLAGUE.get());
+                player.removeEffect(ModEffects.SALMONELLA.get());
+            }
+            
+        }
+    }
+
+    // Cure bacterial infections with infernicillin
+    @SubscribeEvent
+    public static void onInfernicillinConsumed(LivingEntityUseItemEvent.Finish event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+    
+        // Check infernicillin 
+        if (event.getItem().getItem() == ModItems.INFERNICILLIN.get()) {
+
+            // 40% chance
+            if (player.level().random.nextFloat() < 0.40F) {
+                player.removeEffect(ModEffects.ANTHRAX.get());
+                player.removeEffect(ModEffects.DYSENTERY.get());
+                player.removeEffect(ModEffects.PLAGUE.get());
+                player.removeEffect(ModEffects.SALMONELLA.get());
+                player.removeEffect(ModEffects.MALARIA.get());
+            }
+            
+        }
+    }
+
+    // Cure malaria with tonic 
+    @SubscribeEvent
+    public static void onTonicConsumed(LivingEntityUseItemEvent.Finish event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+    
+        // Check infernicillin 
+        if (event.getItem().getItem() == ModItems.TONIC.get()) {
+
+            // 20% chance
+            if (player.level().random.nextFloat() < 0.20F) {
+                player.removeEffect(ModEffects.MALARIA.get());
+            }
+            
+        }
+    }
+    
+}
