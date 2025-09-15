dab92b6488f25d6726ef03478bb3273d3add54be
HOLSEN MOORE <hols3nmoore@gmail.com>
2025-09-14 19:48:04 -0500

    Import ServerPlayer

--- src/main/java/com/holsfastagz/plaguedelight/events/MedicineEvents.java
+++ src/main/java/com/holsfastagz/plaguedelight/events/MedicineEvents.java
@@ -11,6 +11,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
+import net.minecraft.server.level.ServerPlayer;
 
 @Mod.EventBusSubscriber(modid = "plaguedelight")
 public class MedicineEvents {
